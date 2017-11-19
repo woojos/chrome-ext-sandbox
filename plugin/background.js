21b57bd7b9aaab2300cd3b9ad6635367d2b5098a
Marcin Wójcik <marcin.wojcik@getresponse.com>
2017-11-19 22:02:48 +0100

    feature: pass data about http method

--- plugin/background.js
+++ plugin/background.js
@@ -1,4 +1,6 @@
+console.log('dupa');
 
+/*
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 
     if (changeInfo.status === 'complete' && tab.active) {
@@ -7,11 +9,31 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 
             if (items.isLoggedIn) {
                 var xhr = new XMLHttpRequest();
-                xhr.open("GET", "http://127.0.0.1:8888/save-url.php?url=" + tab.url + ':' + items.user, true);
+                xhr.open("GET", "http://127.0.0.1:8888/save-url.php?url=" + tab.url + '|' + items.user, true);
                 xhr.send();
             }
-
-           console.log(items);
         });
     }
 });
+*/
+
+var requestFilter = {urls: ["<all_urls>"]};
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function (details) {
+
+        if (details.type === "main_frame") {
+
+            chrome.storage.local.get(['isLoggedIn', 'user'], function (items) {
+
+                if (items.isLoggedIn) {
+                    console.log(details);
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", "http://127.0.0.1:8888/save-url.php?url=" + details.url + '|' + details.method + '|' + items.user, true);
+                    xhr.send();
+                }
+            });
+        }
+    },
+    requestFilter
+);
